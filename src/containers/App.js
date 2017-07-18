d793f1e07bcbc172fc3df5885c48e243eab5ac21
Antifreez <antifreez@gmail.com>
2017-07-18 21:43:16 +0300

    Starting Store configuration

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import configureStore from './store/configureStore'
 
-export default class App extends Component {
-  render() {
-    return <div>Привет из App!!!</div>
-  }
-}
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
